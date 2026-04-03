b8fa3051514c91ef45c5fdc8a7b2bcdc39fd549d
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2026-04-03 23:41:45 +0800

    adding api

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express"
 
 const app = express()
 
+app.get("/api/notes", (req,res) => {
+    res.status(200).send("you got sessions");
+});
+
 app.listen(5001, () => {
     console.log("Server started on port 5001");
 });
