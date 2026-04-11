d6552e7065c5d001a885db60961d04c6bfdf43ab
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2026-04-11 16:19:43 +0800

    DB connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 import express from "express"
 import sessionRoutes from "./routes/sessionRoutes.js"
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
 
 const app = express()
+const PORT = process.env.PORT || 5001 
+
+connectDB();
 
 app.use("/api/session", sessionRoutes);
 
 
-app.listen(5001, () => {
-    console.log("Server started on port 5001");
+app.listen(PORT, () => {
+    console.log("Server started on port:", PORT);
 });
+
+
+
