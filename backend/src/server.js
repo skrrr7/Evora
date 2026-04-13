e35ebe671d7f6a8244ad41b0afcc0e459e73b37f
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2026-04-13 14:06:58 +0800

    Added RateLimiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import sessionRoutes from "./routes/sessionRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
+import ratelimit from "./config/upstash.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -10,17 +12,21 @@ console.log(process.env.MONGO_URI);
 const app = express()
 const PORT = process.env.PORT || 5001 
 
-connectDB();
 
 //middleware
 app.use(express.json());
+app.use(rateLimiter);
+
 
 app.use("/api/session", sessionRoutes);
 
 
-app.listen(PORT, () => {
-    console.log("Server started on port:", PORT);
-});
+connectDB().then (() => {
+    app.listen(PORT, () => {
+       console.log("Server started on port:", PORT);
+    });
+})
+
 
 
 
