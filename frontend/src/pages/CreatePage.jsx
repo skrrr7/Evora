8bd363284df7cbbdc4ac0039ee1af3112884be89
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2026-04-25 01:15:54 +0800

    Added ui createPage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router";
 
 const CreatePage = () => {
-  return <div>CreatePage</div>
+  const {title, setTitle} = useState("");
+  const {content, setContent} = useState("");
+  const {loading, setLoading} = useState(false);
+
+  const handleSubmit = () => {
+
+
+  }
+  return <div className="min-h-screen bg-base-200">
+    <div className="container mx-auto px-4 py-8">
+      <div className="max-w-2xl mx-auto"></div>
+      <Link to={"/"} className="btn btn-ghost mb-6">
+        <ArrowLeftIcon className="size-5" />
+        Back to Session
+      </Link>
+
+      <div className="card bg-base-100">
+        <div className="card-body">
+          <h2 className="card-title text-2xl mb-4">Create New Session</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-control mb-4">
+              <label className="label">
+                  <span className="label-text">Title</span>
+              </label>
+              <input type="text"
+                placeholder="Session Title"
+                className="input input-bordered"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)} 
+              
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
   
 };
 
