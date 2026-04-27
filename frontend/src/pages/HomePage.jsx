de9d4e4a972e59f763159a886c1cd468a2dd8141
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2026-04-27 23:43:07 +0800

    Delete function

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -43,10 +43,12 @@ const HomePage = () => {
     <div className="max-w-7xl mx-auto p-4 mt-6">
       {loading && <div className="text-center text-primary py-10">Loading session...</div>}
 
+      {session.length === 0 && !setIsRateLimited && <SessionNotFound/>}
+
       {session.length > 0 && !rateLimited && (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {session.map((session) => (
-            <SessionCard key={session._id} session={session} />
+            <SessionCard key={session._id} session={session} setSession={setSession} />
           ))}
 
         </div>
