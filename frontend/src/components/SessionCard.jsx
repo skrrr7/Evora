4fe7e37316fea341795c41171b17eadb8985a273
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2026-04-27 15:49:52 +0800

    Added Delete

--- frontend/src/components/SessionCard.jsx
+++ frontend/src/components/SessionCard.jsx
@@ -2,10 +2,28 @@ import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router";
 import React from "react";
 import { formatDate } from "../lib/utils";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
 
 
-const SessionCard = ({session}) => {
-  return <Link to={`/session/${session._id}`}
+const SessionCard = ({session,setSession}) => {
+
+  const handleDelete = async(e,id) => {
+    e.preventDefault(); //get rid of navigation
+
+    if(!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+        await api.delete(`/session/${id}`);
+        setSession((prev) => prev.filter(session => session._id !== id))
+        toast.success("Session deleted");
+    } catch (error) {
+        console.log("Error in handleDelete",error);
+        toast.error("Failed to delete session");
+    }
+  }
+  return (
+  <Link to={`/session/${session._id}`}
     className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]"
   >
     <div className="card-body">
@@ -17,7 +35,7 @@ const SessionCard = ({session}) => {
             </span>
             <div className="flex items-center gap-1">
                     <PenSquareIcon className="size-4"/>
-                <button className='btn btn-ghost btn-xs text-error'>
+                <button className="btn btn-ghost btn-xs text-error" onClick={(e) => handleDelete(e,session._id)}>
                     <Trash2Icon classname="size-4" />
                 </button>
 
@@ -26,6 +44,7 @@ const SessionCard = ({session}) => {
     </div>
 
   </Link>
+  )
 };
 
 export default SessionCard
