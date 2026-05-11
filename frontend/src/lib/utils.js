86a2f6cf353bfa11717773970454036092c5d14a
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2026-05-11 20:26:39 +0800

    utils

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,7 +1,17 @@
 export function formatDate(date) {
-    return date.toLocaleDateString("en-US",{
+    return date.toLocaleDateString("en-US", {
         month: "short",
         day: "numeric",
         year: "numeric",
+    });
+}
+
+export function formatDateTime(date) {
+    return date.toLocaleString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "2-digit",
     });
 }
