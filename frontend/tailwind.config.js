0317d341727ccb231075316f7c6b18e8303b3aa2
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2026-05-11 20:26:24 +0800

    tailwind fontstyle

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,11 @@ import daisyui from 'daisyui';
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi", "Inter", "system-ui", "sans-serif"],
+      },
+    },
   },
   plugins: [daisyui],
   daisyui:{
