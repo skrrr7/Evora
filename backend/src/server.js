4f01b42f2d851e0b7b67d14e6fdcfa82840044c0
Raphael Jay Escosia <escosiaraphael70@gmail.com>
2026-06-02 00:40:49 +0800

    V1

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors";
+import path from "path";
 
 import sessionRoutes from "./routes/sessionRoutes.js"
 import { connectDB } from "./config/db.js";
@@ -12,21 +13,31 @@ dotenv.config();
 console.log(process.env.MONGO_URI);
 
 const app = express()
-const PORT = process.env.PORT || 5001 
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); 
 
 
 //middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if(process.env.NODE_ENV !== "production"){
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 
-
 app.use("/api/session", sessionRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend", "dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+    });
+} 
 
 
 connectDB().then (() => {
